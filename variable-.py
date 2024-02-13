3fdbd75ad684ceda98b5c3a5fceda6e36ab0170c
iammithun <157653522+iammithun@users.noreply.github.com>
2024-02-12 22:23:07 -0500

    Add files via upload

---
+++ variable-.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 23 16:45:26 2024
+
+@author: iamrs
+"""
+
+input("what is your name?")
+name=input("what is your name?")
+print(name)
+name="Mithun"
+print(name)
+name="Omkar"
+print(name)
+name=input("what is your name")
+lenght=len(name)
+print(lenght)
